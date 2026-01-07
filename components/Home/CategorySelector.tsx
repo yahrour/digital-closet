9444985c99ff68aca526409682cca057ef0b7477
0x0bito <iskeyram@gmail.com>
2026-01-07 23:16:40 +0100

    Add multi select for home page

--- components/Home/CategorySelector.tsx
+++ components/Home/CategorySelector.tsx
@@ -1,60 +1,73 @@
 import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectLabel,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
+  MultiSelect,
+  MultiSelectContent,
+  MultiSelectGroup,
+  MultiSelectItem,
+  MultiSelectTrigger,
+  MultiSelectValue,
+} from "@/components/ui/multi-select";
 
 export default function CategorySelector() {
   return (
-    <div className="border-b border-dashed pb-2 flex justify-between items-center">
-      <Select>
-        <SelectTrigger className="w-45">
-          <SelectValue>Category</SelectValue>
-        </SelectTrigger>
-        <SelectContent className="max-h-1000">
-          <SelectGroup>
-            <SelectLabel>Categories</SelectLabel>
-            <SelectItem value="one">1</SelectItem>
-          </SelectGroup>
-        </SelectContent>
-      </Select>
-      <Select>
-        <SelectTrigger className="w-45">
-          <SelectValue>Season</SelectValue>
-        </SelectTrigger>
-        <SelectContent className="max-h-1000">
-          <SelectGroup>
-            <SelectLabel>Seasons</SelectLabel>
-            <SelectItem value="one">1</SelectItem>
-          </SelectGroup>
-        </SelectContent>
-      </Select>
-      <Select>
-        <SelectTrigger className="w-45">
-          <SelectValue>Color</SelectValue>
-        </SelectTrigger>
-        <SelectContent className="max-h-1000">
-          <SelectGroup>
-            <SelectLabel>Colors</SelectLabel>
-            <SelectItem value="one">1</SelectItem>
-          </SelectGroup>
-        </SelectContent>
-      </Select>
-      <Select>
-        <SelectTrigger className="w-45">
-          <SelectValue>Tag</SelectValue>
-        </SelectTrigger>
-        <SelectContent className="max-h-1000">
-          <SelectGroup>
-            <SelectLabel>Tags</SelectLabel>
-            <SelectItem value="one">1</SelectItem>
-          </SelectGroup>
-        </SelectContent>
-      </Select>
+    <div className="border-b border-dashed pb-2 flex justify-between items-center w-full md:gap-12 sm:gap-8 max-sm:gap-4">
+      <MultiSelect>
+        <MultiSelectTrigger className="flex-1">
+          <MultiSelectValue
+            overflowBehavior="cutoff"
+            placeholder="Categories"
+          />
+        </MultiSelectTrigger>
+        <MultiSelectContent>
+          <MultiSelectGroup>
+            <MultiSelectItem value="next.js">Next.js</MultiSelectItem>
+            <MultiSelectItem value="sveltekit">SvelteKit</MultiSelectItem>
+            <MultiSelectItem value="astro">Astro</MultiSelectItem>
+            <MultiSelectItem value="vue">Vue.js</MultiSelectItem>
+          </MultiSelectGroup>
+        </MultiSelectContent>
+      </MultiSelect>
+
+      <MultiSelect>
+        <MultiSelectTrigger className="flex-1">
+          <MultiSelectValue overflowBehavior="cutoff" placeholder="Seasons" />
+        </MultiSelectTrigger>
+        <MultiSelectContent>
+          <MultiSelectGroup>
+            <MultiSelectItem value="next.js">Next.js</MultiSelectItem>
+            <MultiSelectItem value="sveltekit">SvelteKit</MultiSelectItem>
+            <MultiSelectItem value="astro">Astro</MultiSelectItem>
+            <MultiSelectItem value="vue">Vue.js</MultiSelectItem>
+          </MultiSelectGroup>
+        </MultiSelectContent>
+      </MultiSelect>
+
+      <MultiSelect>
+        <MultiSelectTrigger className="flex-1">
+          <MultiSelectValue overflowBehavior="cutoff" placeholder="Colors" />
+        </MultiSelectTrigger>
+        <MultiSelectContent>
+          <MultiSelectGroup>
+            <MultiSelectItem value="next.js">Next.js</MultiSelectItem>
+            <MultiSelectItem value="sveltekit">SvelteKit</MultiSelectItem>
+            <MultiSelectItem value="astro">Astro</MultiSelectItem>
+            <MultiSelectItem value="vue">Vue.js</MultiSelectItem>
+          </MultiSelectGroup>
+        </MultiSelectContent>
+      </MultiSelect>
+
+      <MultiSelect>
+        <MultiSelectTrigger className="flex-1">
+          <MultiSelectValue overflowBehavior="cutoff" placeholder="Tags" />
+        </MultiSelectTrigger>
+        <MultiSelectContent>
+          <MultiSelectGroup>
+            <MultiSelectItem value="next.js">Next.js</MultiSelectItem>
+            <MultiSelectItem value="sveltekit">SvelteKit</MultiSelectItem>
+            <MultiSelectItem value="astro">Astro</MultiSelectItem>
+            <MultiSelectItem value="vue">Vue.js</MultiSelectItem>
+          </MultiSelectGroup>
+        </MultiSelectContent>
+      </MultiSelect>
     </div>
   );
 }
