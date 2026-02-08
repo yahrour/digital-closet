72d766004b9559d848d022481a3b880904015981
0x0bito <iskeyram@gmail.com>
2026-02-08 15:24:00 +0100

    Install eslint and prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,19 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import prettierConfig from "eslint-plugin-prettier/recommended";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  prettierConfig,
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
     "build/**",
-    "next-env.d.ts",
+    "next-env.d.ts",    
   ]),
 ]);
 
